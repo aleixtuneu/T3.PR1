c717c02295b964487a63118235516a4136773a4c
aleixtuneu <aleix.tuneu.7e7@itb.cat>
2025-01-26 23:30:12 +0100

    feature: EnergySystem abstract class #1

---
+++ T3.Pr1/T3.Pr1/AEnergySystem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace T3.Pr1
+{
+    public abstract class AEnergySystem : IEnergyCalculus
+    {
+        public string Type { get; set; }
+        public DateTime SimulationDate { get; set; }
+        protected double GeneratedEnergy { get; set; }
+
+        public AEnergySystem()
+        {
+            SimulationDate = DateTime.Now;
+        }
+
+        public abstract void ConfigurateParameters();
+        public abstract double CalculateEnergy();
+    }
+}
+

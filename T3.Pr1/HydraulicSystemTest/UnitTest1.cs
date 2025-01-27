3c12bba7e27bd0a35990ec3141afb8342bdc13e4
aleixtuneu <aleix.tuneu.7e7@itb.cat>
2025-01-27 19:52:52 +0100

    feature: HidrauliSystemTests #14

---
+++ T3.Pr1/HydraulicSystemTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace HydraulicSystemTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ T3.Pr1/Pr1.Tests/HidraulicSystemTests.cs
@@ -0,0 +1,37 @@
+using T3.Pr1;
+using Xunit;
+
+namespace Pr1.Tests
+{
+    public class HidraulicSystemTests
+    {
+        [Fact]
+        public void ConfigurateParameters_ShouldSetValidWaterFlow()
+        {
+            // Arrange
+            var system = new HidraulicSystem();
+            var validWaterFlow = 50; // Un valor válido
+
+            // Act
+            system.SetWaterFlow(validWaterFlow);
+
+            // Assert
+            Assert.Equal(validWaterFlow, system.GetWaterFlow());
+        }
+
+        [Fact]
+        public void CalculateEnergy_ShouldReturnExpectedEnergy()
+        {
+            // Arrange
+            var system = new HidraulicSystem();
+            system.SetWaterFlow(20);
+
+            // Act
+            var energy = system.CalculateEnergy();
+
+            // Assert
+            var expectedEnergy = 20 * 9.8 * 9.8;
+            Assert.Equal(expectedEnergy, energy, 2);
+        }
+    }
+}
